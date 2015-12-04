b5c6bf7fed8956c580f2aa9396dd61cb3af6d256
dylan <dtzitz@live.com>
2015-12-04 13:01:41 -0500

    adds weather regex and performance updates

--- server/bot/warmInput.js
+++ server/bot/warmInput.js
@@ -7,21 +7,15 @@ Router.route('twilio/endpoint',{where:'server'})
 		var request = this.request.body;
 		console.log(request.From);
 		
-		var xmlData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-		xmlData += "<Response>";
-		xmlData += "<Message>"+request.From+"</Message>";
-		xmlData += "</Response>";
+		
+		//should be:
+		var xmlData = Meteor.call('warmProcess',request)
 		
 		this.response.writeHead(200,{'Content-Type':'application/xml'});
 		this.response.end(xmlData);
 	});
 	
-// var date = new Date();//consider bringing in moment.js for more complex date time interactions
-// var messageSid = routeContext.params.query.MessageSid;
-// var accountSid = routeContext.params.query.AccountSid;
-// var fromNumber = routeContext.params.query.From;
-// var toNumber = routeContext.params.query.To;
-// var messageBody = routeContext.params.query.Body;
+
 
 // if (!fromNumber){
 // 	return 'Error - No from number';
---
+++ server/bot/warmProcess.js
@@ -0,0 +1,38 @@
+
+Meteor.methods({
+	warmProcess:function(request){
+		
+		var messageText = request.Body.toLowerCase();
+		
+		var weatherRegex = /\b(weather|rain|temperature)\b/;
+		
+		if (weatherRegex.text(messageText)){
+			
+			//should probably call to an actual weather API
+			//using area code for location information
+			var xmlData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+			xmlData += "<Response>";
+			xmlData += "<Message>IT'S GON\' RAIN</Message>";
+			xmlData += "</Response>";
+		
+			return xmlData;
+			
+		}
+		
+		
+	}
+})
+
+// var date = new Date();//consider bringing in moment.js for more complex date time interactions
+// request.MessageSid;
+// request.AccountSid;
+// request.From;
+// request.To;
+// request.Body;
+
+		// var xmlData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+		// xmlData += "<Response>";
+		// xmlData += "<Message>"+request.Body+"</Message>";
+		// xmlData += "</Response>";
+		
+		// return xmlData;
